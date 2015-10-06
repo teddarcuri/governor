6f32a50e8473e39b59d173f2eea18901bc5877cf
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-10-06 10:05:29 -0600

    load javascript libraries

--- docroot/sites/all/themes/hickenlooper/template.php
+++ docroot/sites/all/themes/hickenlooper/template.php
@@ -17,3 +17,18 @@ function hickenlooper_preprocess_page(&$variables) {
  */
 function hickenlooper_preprocess_node(&$variables) {
 }
+
+// Javascripts
+
+// Jquery
+drupal_add_js(drupal_get_path('theme', 'hickenlooper') .'/bower_components/jQuery/dist/jquery.js', array('scope' => 'footer'));
+
+// react
+drupal_add_js(drupal_get_path('theme', 'hickenlooper') .'/bower_components/react/react.js', array('scope' => 'footer'));
+
+// Libraries
+drupal_add_js(drupal_get_path('theme', 'hickenlooper') .'/bower_components/velocity/velocity.min.js', array('scope' => 'footer'));
+drupal_add_js(drupal_get_path('theme', 'hickenlooper') .'/bower_components/velocity/velocity.ui.min.js', array('scope' => 'footer'));
+drupal_add_js(drupal_get_path('theme', 'hickenlooper') .'/bower_components/slick-carousel/slick/slick.min.js', array('scope' => 'footer'));
+drupal_add_js(drupal_get_path('theme', 'hickenlooper') .'/bower_components/jquery-gi-thewall/jQuery.GI.TheWall.js', array('scope' => 'footer'));
+drupal_add_js(drupal_get_path('theme', 'hickenlooper') .'/js/scripts.js', array('scope' => 'footer'));
